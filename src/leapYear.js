5df143774756a8337b68e81e32fb495178f81f83
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-08-10 20:35:19 +0100

    refactor

--- src/leapYear.js
+++ src/leapYear.js
@@ -1,10 +1,8 @@
 function leapYear(year){
-  if((year % 100 === 0) && (year % 400 != 0)){
-  return false
-  } else if(year % 4 === 0 ) { 
-    return true
-  } else {
-    return false
-  };
+
+
+  if (year % 400 === 0) return true;
+  if (year % 100 === 0) return false;
+  return year % 4 === 0;
 
 }
